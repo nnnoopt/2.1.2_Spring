549642d7f6de398c75444792b2529ea3d97725da
nnnoopt <nnnoopt@yandex.ru>
2024-03-18 07:24:14 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean.getMessage());
+
+        Cat bean3 = applicationContext.getBean("cat", Cat.class);
+        Cat bean4 =  applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
+
     }
 }
